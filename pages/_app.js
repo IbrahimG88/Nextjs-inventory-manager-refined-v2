9a285a39bf897355afff93e52ae7d31a57dece9a
IbrahimG88 <goharyibrahim@gmail.com>
2023-08-03 15:03:11 +0300

    "next-auth implementation"

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
+import "../styles/globals.css";
+import NextNProgress from "nextjs-progressbar";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <NextNProgress
+        color="#4fa94d"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+        timeout={3000}
+      />
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
